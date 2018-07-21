2c6419b20833f398a810a8dd15b0aca56d7b0702
Andrew Brainerd <drwb333@gmail.com>
2018-07-21 13:05:22 -0400

    Remove unused components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProfilePage from './components/ProfilePage';
 import NotFound from './components/NotFound';
 import ProfilePageContainer from './components/ProfilePageContainer';
-import OverwatchSelect from './components/OverwatchSelectContainer';
 import './App.css';
 
 export default class App extends Component {
@@ -15,7 +14,6 @@ export default class App extends Component {
             <ProfilePage {...props} userId="Boone-11892" />
           )}/>
           <Route path='/profile/:id' component={ProfilePageContainer} />
-          <Route path='/select/' component={OverwatchSelect} />
           <Route path='*' component={NotFound} />
         </Switch>
       </Router>
